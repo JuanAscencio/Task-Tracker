f34e16b9018e5a1a01b397c749b34c4e374c7267
Juan Ascencio <juan.ascencio.tapia@gmail.com>
2026-01-03 16:04:38 -0600

    Creates and deletes a directory

--- subparser.py
+++ subparser.py
@@ -1,11 +1,12 @@
-# It creates a directory
-# With the following command:
+# It creates or deletes a directory
+# With the following commands:
 # python subparser.py create -d "Directory_name"
+# python subparser.py delete -d "Directory_name"
 import argparse
 import os
 
 parser = argparse.ArgumentParser()
-subparser = parser.add_subparsers()
+subparser = parser.add_subparsers(dest="command")
 
 create_parser = subparser.add_parser(
     "create", help="create directory"
@@ -15,11 +16,25 @@ create_parser.add_argument(
     "-d", nargs=1
 )
 
+delete_parser = subparser.add_parser(
+    "delete", help="create directory"
+)
 
-args = parser.parse_args()
+delete_parser.add_argument(
+    "-d", nargs=1
+)
+
+
+args: argparse.Namespace = parser.parse_args()
 
 print(args)
 
-if args.d:
-    os.mkdir(args.d[0])
-    print("Done")
+if args.command == "create":
+    if args.d:
+        os.mkdir(args.d[0])
+        print("The directory has been created")
+
+if args.command == "delete":
+    os.rmdir(args.d[0])
+    print("The directory has been removed succesfully!")
+
