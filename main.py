14a3185d2bd5c1f235b2e146f002318ca05c20d0
= <=>
2026-01-02 18:50:16 -0600

    Mutually exclusive group

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
+# You run it like this: python main.py 2 5 -vvv
 import argparse
 
 parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
 
 parser.add_argument(
-    "square", help="Squares a given number", 
-    type=int, default=0, nargs="?"
+    "a", type=int, help="The base value"
 )
 
 parser.add_argument(
-    "-v", "--verbose", 
-    help="Verbose description. Use -vv for extra verbose",
-    action="count"
+    "b", type=int, help="The exponent"
+)
+
+group.add_argument(
+    "-v", "--verbose", action="count",
+    help="Provides a verbose description. Use -vv for extra verbose."
+)
+
+group.add_argument(
+    "-s", "--silence", action="store_true",
+    help="Generate a silent version of the output"
 )
 
 args: argparse.Namespace = parser.parse_args()
-result: int = args.square ** 2
+result: int = args.a ** args.b
 
-if args.verbose == 1:
-    print(f"The result is: {result}")
-elif args.verbose == 2:
-    print(f"{args.square} ** {args.square} = {result}")
+if args.silence:
+    print("Silenced!")
 else:
-    print(result)
+    match args.verbose:
+        case 1:
+            print(f"The result is {result}")
+        case 2:
+            print(f"{args.a} ** {args.b} = {result}")
+        case _:
+            print(result)
